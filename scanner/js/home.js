ec9481b9e64e5c4f2d9dd9c9b327093355d19c5b
LionelNoussi <76688083+LionelNoussi@users.noreply.github.com>
2021-01-03 18:03:28 +0100

    Delete home.js

--- scanner/js/home.js
+++
@@ -1,9 +0,0 @@
-var input = document.getElementById('student_name');
-input.focus();
-
-function moveOnMax(field){
-    document.write()
-    if(field.value.length >= 8){
-        document.getElementById(item).focus();
-    }
-}
